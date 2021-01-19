c7c74542c262ff3fb7aa383369322887c63835a3
dyoder838 <70074229+dyoder838@users.noreply.github.com>
2021-01-19 14:11:26 -0800

    finished info pane

--- src/components/InfoPane/index.js
+++ src/components/InfoPane/index.js
@@ -1,27 +1,48 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
   root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    '& > *': {
-      margin: theme.spacing(1),
-      width: theme.spacing(16),
-      height: theme.spacing(16),
-    },
+    minWidth: 275,
+    textAlign: 'center',
   },
-}));
-
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
 export default function InfoPane() {
-  
-    const classes = useStyles();
+  const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <Paper elevation={3} />
-    </div>
+    <Card className={classes.root}>
+      <CardContent>
+
+        <Typography variant="h5" component="h2" className={classes.title}>
+         Welcome!
+        </Typography>
+       
+        <Typography variant="body2" component="p">
+          welcome to our App, it is still under construction; please enjoy watching it grow.
+          <br />
+          Here we will tell you our mission statement. 
+          <br />
+          Here we will tell you our rules. 
+        </Typography>
+
+      </CardContent>
+   
+    </Card>
   );
 }
